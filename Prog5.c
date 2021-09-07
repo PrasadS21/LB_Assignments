5864b5095cd93b3316ceeaf869b02baa0743a0bc
Prasad Sarode <90206669+PrasadS21@users.noreply.github.com>
2021-09-07 18:16:05 +0530

    Add files via upload

---
+++ Prog5.c
@@ -0,0 +1,22 @@
+//5. Accept one number from user and print that number of * on screen.
+
+#include<stdio.h>
+
+	void Accept(int iNo)
+	{
+		int iCnt = 0;
+		for(iCnt=0;iCnt<=iNo;iCnt++)
+		{
+			printf("*");
+		}
+	}
+int main()
+{
+	int iValue = 0;
+	iValue = 5;
+	
+	Accept(iValue);
+		
+	return 0;
+}
+
